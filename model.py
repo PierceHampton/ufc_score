1a6d92f0cdef751cf00cecdfe828b6aedc5bf7eb
PierceHampton <77135850+PierceHampton@users.noreply.github.com>
2025-08-22 11:15:33 -0600

    Removed redundant/moved files

--- model.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("jerzyszocik/ufc-fight-forecast-complete-gold-modeling-dataset")
-
-print("Path to dataset files:", path)
